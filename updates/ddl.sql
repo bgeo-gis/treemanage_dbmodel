f388834f880e8563414a3d1ff51dbf58dbe67a28
Barbara Rzepka <bbrzepka@bgeo.es>
2019-02-04 16:16:51 +0100

    create audit_cat_table

--- updates/ddl.sql
+++ updates/ddl.sql
@@ -14,3 +14,17 @@ CREATE TABLE selector_campaign
   cur_user text,
   CONSTRAINT selector_campaign_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE audit_cat_table (
+    id text NOT NULL,
+    context text,
+    description text,
+    sys_role_id character varying(30),
+    sys_criticity smallint,
+    sys_rows text,
+    qgis_role_id character varying(30),
+    qgis_criticity smallint,
+    qgis_message text,
+    sys_sequence text,
+    sys_sequence_field text
+);
